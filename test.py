94742d2aa4e61b0ce37f9aed145f258fbd8faf01
gowrirvgh <gowrivijayakumar86@gmail.com>
2024-10-23 15:33:18 +0530

    Update test.py

--- test.py
+++ test.py
@@ -15,3 +15,6 @@ print(f"Addition: {num1} + {num2} = {addition}")
 print(f"Subtraction: {num1} - {num2} = {subtraction}")
 print(f"Multiplication: {num1} * {num2} = {multiplication}")
 print(f"Division: {num1} / {num2} = {division:.2f}")  # Format to 2 decimal places
+
+
+hi this is naina biju
